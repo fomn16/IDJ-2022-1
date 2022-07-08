6ba305b0ae3605c360e74b0bd2e247bed60f9172
Felipe Neves <felipe.neves16@gmail.com>
2022-07-08 11:00:36 -0300

    Trabalho 2 parte 5

---
+++ include/Face.hpp
@@ -0,0 +1,21 @@
+#ifndef __FACE_H__
+#define __FACE_H__
+
+#include "Component.hpp"
+#include "GameObject.hpp"
+
+#include <string>
+
+class Face : Component{
+    public:
+        Face(GameObject& associated);
+
+        void Damage(int damage);
+
+        void Update(float dt);
+        void Render();
+        bool Is(std::string type);
+    private:
+        int hitpoints;
+};
+#endif
